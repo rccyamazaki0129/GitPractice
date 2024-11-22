876a7ff3a3a10688180e8c3bf120dcb14bdeb1a8
rccyamazaki0129 <rccyamazaki0129@gmail.com>
2024-11-22 21:58:39 +0900

    feat: greeting関数の定義を作成

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ console.log("Hello, world!");
 // マジックナンバーを表示する
 const magicNumber = 6478293828;
 console.log(`magic number : ${magicNumber}`);
+
+// 名前を指定して，日本語で挨拶する関数
+const greeting = (name) => {
+
+}
